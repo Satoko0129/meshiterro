4b47c9729f13cf76b87dacd6457d96a560b068cb
EC2 Default User <ec2-user@ip-172-31-22-93.ap-northeast-1.compute.internal>
2023-05-24 13:32:41 +0000

    NameError未解決

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'users/new'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "homes#top"
+get "homes/about" => "homes_about"
+resources :users
 end
